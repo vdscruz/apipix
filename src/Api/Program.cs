7650aea747e55ad3e2be763ab1a9481432edd418
Vinicius Cruz <viniciuscruz.desen@gmail.com>
2022-08-26 22:49:58 -0300

    criando ValidationExtension

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,14 +1,11 @@
 global using FastEndpoints;
 using Api.Validation;
-using Api.Validation.Loc;
-using Application.Model.Loc.Requests;
-using FluentValidation;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddFastEndpoints()
-    .AddScoped<IValidator<CriarLocationRequest>, CriarLocationRequestValidator>();
+    .AddValidators();
 
 
 var app = builder.Build();
---
+++ src/Api/Validation/ValidationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Api.Validation.Loc;
+using Application.Model.Loc.Requests;
+using FluentValidation;
+
+namespace Api.Validation;
+
+public static class ValidationExtension
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IValidator<CriarLocationRequest>, CriarLocationRequestValidator>();
+        
+        return services;
+    }
+}
